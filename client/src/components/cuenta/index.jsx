c7842d0ed540e868b504a88e72f20399a189eac4
facuzer <facundozerbino@gmail.com>
2020-11-16 13:43:52 -0300

    boton salir

--- client/src/components/cuenta/index.jsx
+++ client/src/components/cuenta/index.jsx
@@ -1,20 +1,27 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types'
 import { Grid } from '@material-ui/core'
+import { Redirect } from 'react-router-dom';
 import { request } from '../../components/helpers/request'
 import qs from 'qs'
 
 export default class DetallesCuenta extends Component {
     static propTypes = {
-        //charlar con valen sobre que se le va a pasar para buscar el user
         id: PropTypes.number.isRequired,
         token: PropTypes.string.isRequired
     }
     constructor(props) {
         super(props)
         this.state={
-            user: null
+            user: null,
+            redirect:false
         }
+        this.clearAndRedirect = this.clearAndRedirect.bind(this);
+    }
+
+    clearAndRedirect(event){
+        localStorage.clear()
+        this.setState({...this.state, redirect:true})
     }
 
     async componentDidMount() {
@@ -29,6 +36,9 @@ export default class DetallesCuenta extends Component {
 
     render(){
         return(
+            this.state.redirect == true ?
+                <Redirect to="/"></Redirect>
+            :
             this.state.user ? 
             <Grid className="media mb-3" container justify="center" alignItems="center" alignContent="center">
                 <Grid item lg={3} xs={12} md={3}>
@@ -40,6 +50,9 @@ export default class DetallesCuenta extends Component {
                         <h3 className="font-weight-bold col-10"> Usuario: <span className="font-weight-normal"> {this.state.user.usuario}</span></h3>
                         <h3 className="font-weight-bold col-10"> Mail: <span className="font-weight-normal"> {this.state.user.mail}</span></h3>
                     </Grid>
+                </Grid>
+                <Grid item lg={6} xs={12} md={3}>
+                    <button className="btn btn-primary mt-2 ml-4" onClick={this.clearAndRedirect}> Salir </button>
                 </Grid>
             </Grid>
             :
